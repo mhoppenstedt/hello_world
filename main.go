1a22b70fc52afbed11b375429a03439f96b670cc
Mitchell Hoppenstedt <hoppenstedt.mitch@gmail.com>
2018-07-13 08:11:20 -0500

    This is the second time i am commiting a file.

--- main.go
+++ main.go
@@ -8,4 +8,9 @@ import (
 func main() {
 	fmt.Println("hello world")
 	fmt.Println(math.Cos(math.Pi))
+	fmt.Println("Rectangle area: ", rectArea(5, 5))
+}
+
+func rectArea(x, y int) int {
+	return x * y
 }
